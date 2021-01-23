43d4647606ca21195fb0f2246334b55701892a26
Aris Ripandi <ar.is@outlook.com>
2021-01-23 21:19:20 +0700

    Update Service Provider

--- src/Providers/MidtransServiceProvider.php
+++ src/Providers/MidtransServiceProvider.php
@@ -7,28 +7,14 @@ use Illuminate\Support\ServiceProvider;
 
 class MidtransServiceProvider extends ServiceProvider {
 
-    /** @return string */
-    public function getConfigPath() {
-        // return __DIR__ . '/../../config/midtrans.php';
-    }
-
     public function boot() {
-        // $this->publishes([
-        //     $this->getConfigPath() => config_path('midtrans.php'),
-        // ], 'config');
-
-        // $this->app->singleton('midtrans', function ($app) {
-        //     return new Midtrans;
-        // });
-
-        // Midtrans::registerMidtransConfig();
+        $this->app->singleton('midtrans', function ($app) {
+            return new Midtrans;
+        });
     }
 
     public function register() {
-        // $this->mergeConfigFrom(
-        //     $this->getConfigPath(),
-        //     'midtrans'
-        // );
+        //
     }
 
 }
